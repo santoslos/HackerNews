53c506437512a2820e1a9d3dd7fa18ea82ab96fa
santoslos <papoyan.gosha@gmail.com>
2021-07-30 13:11:55 +0300

    дДобавил loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,41 @@ import React from 'react';
 import './App.css';
 import { StoreNews } from './hooks/store';
 import NewsItem from './components/NewsItem';
+import Prealoader from './components/common/Preloader/Preloader';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
-  const { orderedNews, setNewPage } = StoreNews();
+    const {orderedNews, setNewPage} = StoreNews();
 
-  return (
-    <div className="App">
-      <button
-        onClick={() => {
-          setNewPage(2);
-        }}
-      >
-        click
-      </button>
-      <ul>
-        {orderedNews.map((newsItem) => (
-          <NewsItem
-            key={newsItem.id}
-            title={newsItem.title}
-            points={newsItem.points}
-            user={newsItem.user}
-            timeAgo={newsItem.time_ago}
-          />
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            {!orderedNews.length ? <Prealoader/>
+                :
+                (
+                    <div className="App">
+                        <button
+                            onClick={() => {
+                                setNewPage(2);
+                            }}
+                        >
+                            click
+                        </button>
+                        <ul>
+                            {orderedNews.map((newsItem) => (
+                                <NewsItem
+                                    key={newsItem.id}
+                                    title={newsItem.title}
+                                    points={newsItem.points}
+                                    user={newsItem.user}
+                                    timeAgo={newsItem.time_ago}
+                                />
+                            ))}
+                        </ul>
+                    </div>
+                )}
+        </div>
+    );
 }
 
 export default App;
